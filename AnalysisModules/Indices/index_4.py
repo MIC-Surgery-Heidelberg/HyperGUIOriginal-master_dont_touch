b800d44509b976020d9bca050435b838beb1c174
MIC-Surgery-Heidelberg <67436320+MIC-Surgery-Heidelberg@users.noreply.github.com>
2020-08-09 18:33:50 +0200

    Add files via upload

--- AnalysisModules/Indices/index_4.py
+++ AnalysisModules/Indices/index_4.py
@@ -1,14 +1,4 @@
 # Example index 4
-import numpy as np
-
 def get_index_4(x):
-
-    abl = np.gradient(x, axis=2)
-    nominator = np.max(abl[:,:,64:68], axis=2)*1000
-    val710 = x[:,:,42]
-    val900 = x[:,:,80]
-    denominator = np.square(((val710 - val900)*100))
-    
-    
-    index = nominator  / denominator
+    index = x[:, :, 0]
     return index
